0636650b443e81f5a6d76923470ba5a8ff8aee60
anaellezou <anaelle.zouari@gmail.com>
2018-05-17 18:28:31 +0200

    flying away

--- main.go
+++ main.go
@@ -14,7 +14,13 @@ func HomePage(w http.ResponseWriter, r *http.Request) {
 	t.Execute(w, nil)
 }
 
+func ServeStatic(w http.ResponseWriter, r *http.Request) {
+	w.writeHeader(http.StatusOk)
+	io.WriteString(w, "Hello")
+}
+
 func main() {
 	http.HandleFunc("/", HomePage)
+	http.HandleFunc("/static", ServeStatic)
 	http.ListenAndServe(":4242", nil)
 }
